55f525cf561aae2f504d9f11c2ada464780bb304
Afrographix <bruneltesse@gmail.com>
2025-07-31 19:02:00 +0100

    Working on number generator

--- app/src/main/java/com/example/greetingapp/LuckyNumber.kt
+++ app/src/main/java/com/example/greetingapp/LuckyNumber.kt
@@ -1,12 +1,18 @@
 package com.example.greetingapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class LuckyNumber : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,27 @@ class LuckyNumber : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val luckyNumText:TextView = findViewById(R.id.luckyNumText);
+        val shareBtn:Button = findViewById(R.id.shareBtn);
+        val userName:TextView = findViewById(R.id.userName);
+
+        var username = receiveUserName();
+        userName.text = username;
+
+        var randomNum = generateRandomNum();
+        luckyNumText.text = randomNum.toString();
+    }
+
+
+    fun receiveUserName():String{
+        var bundle:Bundle? = intent.extras;
+        var username = bundle?.get("name").toString();
+        return username;
+    }
+
+    fun generateRandomNum():Int{
+        val random = Random.nextInt(1000);
+        return random;
     }
 }
--- app/src/main/java/com/example/greetingapp/MainActivity.kt
+++ app/src/main/java/com/example/greetingapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wishBtn:Button = findViewById(R.id.wish_btn)
+        val welcomeText:TextView = findViewById(R.id.welcomeText);
+        val nameInput:EditText = findViewById(R.id.nameInput);
+
+
+        wishBtn.setOnClickListener(){
+            val name = nameInput.text
+
+            //Explicit Intent
+            var i:Intent = Intent(this,LuckyNumber::class.java);
+            i.putExtra("name",name);
+            startActivity(i);
+        }
+
+
+
 
 //       var btn:Button = findViewById(R.id.btn);
 //        btn.setOnClickListener(){
